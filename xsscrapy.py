c559d483e94e52c29a41092a5f2e55059c4cbf19
Dan McInerney <danhmcinerney@gmail.com>
2014-11-09 20:28:37 -0500

    readded rate limiting

--- xsscrapy.py
+++ xsscrapy.py
@@ -15,18 +15,24 @@ def get_args():
     parser.add_argument('-u', '--url', help="URL to scan; -u http://example.com")
     parser.add_argument('-l', '--login', help="Login name; -l danmcinerney")
     parser.add_argument('-p', '--password', help="Password; -p pa$$w0rd")
-    parser.add_argument('-c', '--connections', default='30', help="Set the max number of simultaneous connections allowed")
+    parser.add_argument('-c', '--connections', default='30', help="Set the max number of simultaneous connections allowed, default=30")
+    parser.add_argument('-r', '--ratelimit', default='0', help="Rate in requests per minute, default=0")
     parser.add_argument('--basic', help="Use HTTP Basic Auth to login", action="store_true")
     args = parser.parse_args()
     return args
 
 def main():
     args = get_args()
+    rate = args.rate
+    if rate is not None:
+        delay = str(60 / float(rate))
     try:
         execute(['scrapy', 'crawl', 'xsscrapy', 
                  '-a', 'url=%s' % args.url, '-a', 'user=%s' % args.login, '-a', 
-                 'pw=%s' % args.password, '-s', 'CONCURRENT_REQUESTS=%s' % args.connections, 
-                 '-a', 'basic=%s' % args.basic])
+                 'pw=%s' % args.password, '-a', 'basic=%s' % args.basic, '-s', 
+                 'CONCURRENT_REQUESTS=%s' % args.connections,
+                 'DOWNLOAD_DELAY=%s' % rate]
+                 ])
     except KeyboardInterrupt:
         sys.exit()
 
