b199bcf98146ef29abf732df82646a0c9b88ea87
David Dworken <david@daviddworken.com>
2016-10-24 21:20:33 -0400

    Switched to scrapy 1.1.0rc3 to fix SSL bug
    Scrapy prior to 1.1.0rc3 had an issue where it would fail to connect to certain servers with an error stating ```<twisted.python.failure.Failure OpenSSL.SSL.Error: ('SSL routines', 'SSL3_READ_BYTES', 'sslv3 alert handshake failure'), ('SSL routines', 'SSL3_WRITE_BYTES', 'ssl handshake failure')>```. See https://github.com/scrapy/scrapy/issues/1764 for more info on this issue.
    
    In that github issue, there were two potential solutions. Either add additional code forcing it to the correct code (see https://github.com/scrapy/scrapy/issues/1764#issuecomment-181950638). The other potential solution was upgrading to the latest release of scrapy.
    
    There were pretty minimal changes needed to add support for 1.1.0rc3 (just changing import locations) so I went for that.

--- xsscrapy/spiders/xss_spider.py
+++ xsscrapy/spiders/xss_spider.py
@@ -1,7 +1,7 @@
 # -- coding: utf-8 --
 
-from scrapy.contrib.linkextractors import LinkExtractor
-from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 from scrapy.http import FormRequest, Request
 from scrapy.selector import Selector
 from xsscrapy.items import inj_resp
